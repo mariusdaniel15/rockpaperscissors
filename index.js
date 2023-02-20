50914a1be12fe23f2a67ce5d0a0e7f60d339da32
mariusdaniel15 <116435962+mariusdaniel15@users.noreply.github.com>
2023-02-20 22:38:22 +0200

    add function for round, game and show score

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const options = ["rock", "paper", "scissors"];
 
 function getComputerChoice(){
-    const choice = options[Math.floor(Math.random() * options.length)]
+    let choice = options[Math.floor(Math.random() * options.length)]
     return choice;
 }
 
@@ -15,30 +15,78 @@ function getUserChoice(){
 
 function playRound(playerSelection, computerSelection){
     alert("computer choose " + computerSelection);
+    let result = 3;
     if(playerSelection != computerSelection){
         if((playerSelection == "paper")) {
             if (computerSelection == "rock"){
-                alert("You won! Paper beats Rock!");}
+                alert("You won! Paper beats Rock!");
+                result=1;}
             else {
-                alert("You lose! Scissors beats Paper!");}
+                alert("You lose! Scissors beats Paper!");
+                result=-1;}
         }
         else if((playerSelection == "rock")){
             if(computerSelection == "scissors"){
-                alert("You won! Rock beats Scissors!");}
+                alert("You won! Rock beats Scissors!");
+                result=1;
+                }
             else { 
-                alert("You lose! Paper beats Rock!");}
+                alert("You lose! Paper beats Rock!");
+                result=-1;}
         }
         else{
             if(computerSelection =="paper"){
-                alert("You won! Scissors beats Paper!");}
+                alert("You won! Scissors beats Paper!");
+                result=1;}
             else {
-                alert("You lose! Rock beats Scissors!");}
-        } 
+                alert("You lose! Rock beats Scissors!");
+                result=-1;}
+        }
+        return result;
     }
     else {
         alert("TIE!");
+        result=0;
+        return result;
     }
 }
 
 
-playRound(getUserChoice(), getComputerChoice());
+function game(){
+    let counterUser = 0;
+    let counterComputer = 0;
+    let counterTie = 0;
+    let r=1;
+    for(let i = 0; i <= 4; i++){
+        let result = playRound(getUserChoice(), getComputerChoice());
+        console.log("Round "+ r);
+        console.log("User choice " + playRound.getUserChoice);
+        console.log("Computer choice" + playRound.getComputerChoice);
+        if(result == 1) {
+            console.log("You won the round!");
+            ++counterUser;}
+        else if (result == -1){
+            console.log("You lost the round!");
+            ++counterComputer;
+        } 
+        else {
+            console.log("It's a tied round!");
+            ++counterTie;
+        }
+        ++r;
+        //trebuie sa returnez un array cu trei pozitii si fiecare pozitie reprez una dintre counter user etc...
+    }
+}
+
+function showScore(){
+    console.log("Result: ");
+    console.log(game.);
+    if((game.counterTie<=game.counterUser)  || (game.counterTie<=game.counterComputer)){
+        if(game.counterUser>counterComputer) {console.log("You won the game!");}
+        else {console.log("You lost the game!");}
+    }
+    else {console.log("It's a tied game!");}
+    }
+
+game();
+showScore();
